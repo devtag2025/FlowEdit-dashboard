b6789bb2aa77280cbb4491c637346f2088fabcaf
HassanAbbas10 <hassanabbas05674@gmail.com>
2026-04-14 10:48:46 +0500

    modified:   src/app/dashboard/client/service/page.jsx

--- src/app/dashboard/client/service/page.jsx
+++ src/app/dashboard/client/service/page.jsx
@@ -1,10 +1,9 @@
-// src/app/dashboard/client/service/page.jsx
+
 import { Suspense } from "react";
-import ServicePageClient from "./ServicePageClient"; // Import the child component
+import ServicePageClient from "./servicePageClient";
 
 export default function ServicePage() {
   return (
-    // 👇 Wrap the client component in Suspense
     <Suspense fallback={<div className="min-h-screen bg-secondary flex items-center justify-center">Loading services...</div>}>
       <ServicePageClient />
     </Suspense>
